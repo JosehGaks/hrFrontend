2b7c415157eb7ef60c3ee6f9dc50c9db1a714ad2
JosehGaks <josehwilddog@gmail.com>
2022-08-31 18:40:41 +0300

    extend Dataservice in the EmployeeService

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EmployeesService {
+export class EmployeesService extends DataService{
 
-  constructor() { }
+  constructor(http:HttpClient) {
+    super(environment.API,http)
+   }
 }
