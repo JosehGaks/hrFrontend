1fb3f13c0cbb99dbd75d2b9b8f25f7299a190863
JosehGaks <josehwilddog@gmail.com>
2022-09-08 20:25:38 +0300

    create employees data table

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
+import { Employee } from '../models/employee.model';
 import { EmployeesService } from '../services/employees.service';
 
 @Component({
@@ -10,18 +10,47 @@ import { EmployeesService } from '../services/employees.service';
 export class EmployeesComponent implements OnInit {
 
   employees: Employee[] = [];
+  delete: boolean = false;
+  agree: boolean = false;
 
   constructor(private employeeService:EmployeesService) { }
 
   private loadEmployees(){
-     this.employeeService.getAll().subscribe({
-      next: (res) => this.employees = res,
-      error: err => console.log(err)
+    this.employeeService.getAll().subscribe({
+      next : res => {
+        console.log(res.employees);
+        this.employees = res.employees;
+        console.log(this.employees)
+      },
+      error: err => console.error(err) 
     })
+    
+  }
+
+  deleteEmployee(url:string){
+    this.delete = true;
+    console.log(url)
+    
+    if(this.agree){
+      this.delete = false;
+      this.employeeService.deleteByUrl(url).subscribe({
+      next:res => console.log("user deleted successfully"),
+      error: err => {
+        console.error(err)
+      }
+    })
+    }
+
+    this.delete = false;
+    this.loadEmployees()
+  }
+
+  editEmployee(url:string){
+    
   }
 
   ngOnInit(): void {
-    this.loadEmployees();
+    this.loadEmployees()
   }
 
 }
