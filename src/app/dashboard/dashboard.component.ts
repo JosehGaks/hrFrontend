ac2694664353c9c61b1c4505d0584419b1e2f4cc
JosehGaks <josehwilddog@gmail.com>
2022-09-08 20:24:58 +0300

    create employees variable and initalize

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeesService } from '../services/employees.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  employees: Employee[] = [];
+
+  constructor(private employeeService:EmployeesService) { }
 
   ngOnInit(): void {
+    this.employeeService.getAll().subscribe({
+      next : employees => {
+        console.log(employees)
+        this.employees = employees.employees},
+      error: err => console.error(err) 
+    })
   }
 
 }
